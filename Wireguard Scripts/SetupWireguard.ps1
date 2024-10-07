c343a9fb7f2c5e45f87bdb32a57fc3fe281d89fb
Yordan Vuchkov <87576885+Dani5665@users.noreply.github.com>
2024-10-07 16:15:18 +0300

    Update SetupWireguard.ps1

--- Wireguard Scripts/SetupWireguard.ps1
+++ Wireguard Scripts/SetupWireguard.ps1
@@ -15,9 +15,6 @@ if (-not (Is-RunAsAdministrator)) {
     exit
 }
 
-#Set PowerShell Execution Policy
-Set-ExecutionPolicy Restricted
-
 # Get the directory of the current script
 $scriptDirectory = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
