6f4da74d401ffb9d67a0e2e41ca99bbce7a2231b
Yordan Vuchkov <87576885+Dani5665@users.noreply.github.com>
2024-10-09 13:55:09 +0300

    Update MainTaskScript.ps1

--- Wireguard Scripts/MainTaskScript.ps1
+++ Wireguard Scripts/MainTaskScript.ps1
@@ -5,4 +5,4 @@ Start-Process -FilePath "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.e
 
 # Start Script2 in the background, passing the parameters
 $script2Path = "C:\Program Files\WireGuard\WireGuardActivationScript.ps1"  # Modify this path to the actual location of Script2
-Start-Process -FilePath "powershell.exe" -ArgumentList "-File `"$script2Path`" -key `"$key`" -secret `"$secret`" -url `"$url`"" -NoNewWindow -PassThru
+Start-Process -FilePath "powershell.exe" -ArgumentList "-File `"$script2Path`"" -NoNewWindow -PassThru
